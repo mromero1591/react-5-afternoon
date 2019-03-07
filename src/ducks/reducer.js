3a6f5534d256cc2972665f960efc5146b44f5b03
Mark Romero <markromero@marks-mbp.lan>
2019-03-07 00:05:13 -0700

    completed step 6

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,55 @@
-function reducer(){
+const initialState = {
+    loanType: 'Home Purchase',
+    propertyType: 'Single Family Home',
+    city: '',
+    propToBeUsedOn: '',
+    found: "false",
+    realEstateAgent: "false",
+    cost: 0,
+    downPayment: 0,
+    credit: '',
+    history: '',
+    addressOne: '',
+    addressTwo: '',
+    addressThree: '',
+    firstName: '',
+    lastName: '',
+    email: ''
+}
 
+const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
+const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+
+function reducer(state = initialState, action){
+        switch (action.type) {
+            case UPDATE_LOAN_TYPE:
+                return {
+                    ...state,
+                    loanType: action.payload
+                }
+            case UPDATE_PROPERTY_TYPE:
+                return {
+                    ...state,
+                    propertyType: action.payload
+                }
+            default:
+                return state;
+        }
+
+}
+
+export function updateLoanType(loanType) {
+    return {
+        type: UPDATE_LOAN_TYPE,
+        payload: loanType
+    }
+}
+
+export function updatePropertyType(propertyType) {
+    return {
+        type: UPDATE_PROPERTY_TYPE,
+        payload: propertyType
+    }
 }
 
 export default reducer;
