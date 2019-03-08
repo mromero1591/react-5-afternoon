2afaed2895fd16b6fe088d8a302b0fcb3d50ff7e
Mark Romero <markromero@Marks-MacBook-Pro.local>
2019-03-07 18:25:31 -0700

    completed step 8

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -19,6 +19,7 @@ const initialState = {
 
 const UPDATE_LOAN_TYPE = "UPDATE_LOAN_TYPE";
 const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
+const UPDATE_CITY = "UPDATE_CITY";
 
 function reducer(state = initialState, action){
         switch (action.type) {
@@ -31,6 +32,11 @@ function reducer(state = initialState, action){
                 return {
                     ...state,
                     propertyType: action.payload
+                }
+            case UPDATE_CITY:
+                return {
+                    ...state,
+                    city: action.payload
                 }
             default:
                 return state;
@@ -49,6 +55,13 @@ export function updatePropertyType(propertyType) {
     return {
         type: UPDATE_PROPERTY_TYPE,
         payload: propertyType
+    }
+}
+
+export function updateCity(city) {
+    return {
+        type: UPDATE_CITY,
+        payload: city
     }
 }
 
